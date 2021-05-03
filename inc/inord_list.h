ca424aa54a7e0aee9f94bb993487324a8b43ca8c
Hesham Khaled <heshamkhaled13@gmail.com>
2021-05-03 19:00:26 +0200

    added public insert and delete duplicate functions

--- inc/inord_list.h
+++ inc/inord_list.h
@@ -2,8 +2,34 @@
 #define INORD_LIST_H
 
 #include <iostream>
+#include <string>
 
+typedef string  ListElemType;
 
+class List{
+
+    private:
+            struct node{
+                ListElemType data;
+                struct node *next;
+                unsigned int line;
+                unsigned int frequency;
+            };
+            node*    head;
+            node*    tail;
+            node*    current;
+            node*    previous;
+            List();
+            bool InsertAtFirst(const ListElemType data, unsigned int line);
+            bool InsertAtLast(const ListElemType data, unsigned int line);
+            bool InsertAtAny(const ListElemType data, unsigned int location, unsigned int line);
+            ~List();
+    public:
+            bool            Insert(const ListElemType data, unsigned int line);
+            bool            DeleteDuplicate(const ListElemType data);
+            ListElemType    Fetch(ListElemType &data);
+
+};
 
 
 #endif
--- src/inord_list.cpp
+++ src/inord_list.cpp
@@ -1,1 +1,155 @@
 #include "../inc/inord_list.h"
+using namespace std;
+
+static unsigned int nodeCount = 0;
+
+List::List()
+{
+    head = NULL;
+    tail = NULL;
+    current = NULL;
+}
+
+bool List::InsertAtLast (const ListElemType data, unsigned int line)
+{
+    node *link = new node;
+    if(link == NULL)
+        return false;
+
+    link->data = data;
+
+    link->next = NULL;
+
+    if (head = NULL)
+    {
+        head = link;
+        return true;
+    }
+    else
+    {
+        current = head;
+        tail->next = link;
+        tail = link;
+
+        return true;
+    }
+}
+
+bool List::InsertAtFirst (const ListElemType data, unsigned int line)
+{
+    node *link = new node;
+    if (link == NULL)
+        return false;
+
+    link->data = data;
+
+    if (head == NULL)
+        tail = link;
+
+    link->next = head;
+	head = link;
+
+    return true;
+}
+
+bool List::InsertAtAny(const ListElemType data, unsigned int location, unsigned int line)
+{
+    node *link = new node;
+    if (link == NULL)
+        return false;
+
+    link->data = data;
+
+    if (head == NULL || location == 0)
+    {
+        link->next = head;
+	    head = link;
+        return true;
+    }
+
+
+    current = head;
+    for (int Idx = 0 ; Idx<location-1 ; Idx++)
+        current = current->next;
+
+    link->next = current->next;
+    current->next = link;
+    return true;
+}
+
+bool List::Insert (const ListElemType data,unsigned int line)
+{
+    nodeCount++;
+    bool flag;
+    if (head == NULL)
+    {
+        flag = InsertAtLast(data,line);
+        if (flag)
+            return true;
+        else
+            return false;
+    }
+
+    current = head;
+    unsigned int Itr = 0;
+    while (current != NULL && data.compare(current->data) > 0)
+    {
+        current = current->next;
+        Itr++;
+    }
+
+    flag = this->InsertAtAny(data,Itr,line);
+        if (flag)
+            return true;
+        else
+            return false;
+}
+
+bool List::DeleteDuplicate(const ListElemType data)
+{
+    bool firstPass = false;
+    bool flag = false;
+    unsigned int Itr = 0;
+    unsigned int Idx = 0;
+    if (head == NULL)
+        return false;
+    else
+    {
+        current = head;
+        previous = head;
+        while (current != NULL)
+        {
+            if(current->data != data)
+            {
+                current = current->next;
+                Itr++;
+                continue;
+            }
+            
+            if (!firstPass)
+            {
+                firstPass = true;
+                current = current->next;
+                continue;
+            }
+            else
+            {
+                while(Idx<Itr)
+                {
+                    previous = previous->next;
+                        Idx++;
+                }
+                
+                previous->next = current->next;
+                delete current;
+                current = previous->next;
+                nodeCount--;
+                flag = true;
+            }
+        }
+        if (!flag)
+            return true;
+        else
+            return false;       
+    }
+}
