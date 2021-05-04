4b6bf539b76b4e8dbdf22e873d1aa8dc7d96da18
Hesham Khaled <heshamkhaled13@gmail.com>
2021-05-04 04:42:52 +0200

    modified line type

--- inc/inord_list.h
+++ inc/inord_list.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
 typedef string  ListElemType;
@@ -13,22 +15,23 @@ class List{
             struct node{
                 ListElemType data;
                 struct node *next;
-                unsigned int line;
+                string line;
                 unsigned int frequency;
             };
             node*    head;
             node*    tail;
             node*    current;
             node*    current2;
+            node*    current3;
             node*    previous;
             bool InsertAtLast(const ListElemType data, unsigned int line);
             bool InsertAtAny(const ListElemType data, unsigned int location, unsigned int line);
-            bool DeleteDuplicate(const ListElemType data, int *ptr);
+            bool DeleteDuplicate(const ListElemType data, unsigned int &frequency);
     public:
             List();
             ~List();
             bool            Insert(const ListElemType data, unsigned int line);
-            bool            CleanList(void);
+            bool            Clean();
             ListElemType    Fetch(ListElemType &data);
             void            Print();
 
