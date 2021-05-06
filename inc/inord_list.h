8a08ba94d13fdbcadd0b88bde09672defdf01d62
Hesham Khaled <heshamkhaled13@gmail.com>
2021-05-06 18:33:31 +0200

    removed unneccessary members

--- inc/inord_list.h
+++ inc/inord_list.h
@@ -2,38 +2,39 @@
 #define INORD_LIST_H
 
 #include <iostream>
+
 #include <string>
+
 #include <fstream>
+
 #include <vector>
+
 #include <algorithm>
 
 using namespace std;
 
-typedef string  ListElemType;
+typedef string ListElemType;
 
 class List {
 
-private:
-    struct node {
-        ListElemType data;
-        struct node* next;
-        vector <unsigned int>line;
-        unsigned int frequency;
-    };
-    node* head;
-    node* tail;
-    node* current;
-    node* current2;
-    node* current3;
-    node* previous;
-    bool InsertAtAny(const ListElemType data, unsigned int location, unsigned int line);
-    bool DeleteDuplicate(const ListElemType data, unsigned int& frequency);
-public:
-    List();
-    ~List();
-    bool            Insert(const ListElemType data, unsigned int line);
-    bool            Clean();
-    void            Print();
+    private:
+        struct node {
+            ListElemType data;
+            struct node * next;
+            vector < unsigned int > line;
+            unsigned int frequency;
+        };
+        node * head;
+        node * current;
+        bool InsertAtAny(const ListElemType data, unsigned int location, unsigned int line);
+        bool DeleteDuplicate(const ListElemType data, unsigned int & frequency);
+        
+    public:
+        List();
+        ~List();
+        bool Insert(const ListElemType data, unsigned int line);
+        bool Clean();
+        void Print();
 
 };
 #endif
