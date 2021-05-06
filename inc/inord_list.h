718018d798364cd61e28bf87ca047675f00a8d17
Hesham Khaled <heshamkhaled13@gmail.com>
2021-05-06 04:56:15 +0200

    line datatype change

--- inc/inord_list.h
+++ inc/inord_list.h
@@ -4,36 +4,36 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 typedef string  ListElemType;
 
-class List{
+class List {
 
-    private:
-            struct node{
-                ListElemType data;
-                struct node *next;
-                string line;
-                unsigned int frequency;
-            };
-            node*    head;
-            node*    tail;
-            node*    current;
-            node*    current2;
-            node*    current3;
-            node*    previous;
-            bool InsertAtLast(const ListElemType data, unsigned int line);
-            bool InsertAtAny(const ListElemType data, unsigned int location, unsigned int line);
-            bool DeleteDuplicate(const ListElemType data, unsigned int &frequency);
-    public:
-            List();
-            ~List();
-            bool            Insert(const ListElemType data, unsigned int line);
-            bool            Clean();
-            ListElemType    Fetch(ListElemType &data);
-            void            Print();
+private:
+    struct node {
+        ListElemType data;
+        struct node* next;
+        vector <unsigned int>line;
+        unsigned int frequency;
+    };
+    node* head;
+    node* tail;
+    node* current;
+    node* current2;
+    node* current3;
+    node* previous;
+    bool InsertAtAny(const ListElemType data, unsigned int location, unsigned int line);
+    bool DeleteDuplicate(const ListElemType data, unsigned int& frequency);
+public:
+    List();
+    ~List();
+    bool            Insert(const ListElemType data, unsigned int line);
+    bool            Clean();
+    void            Print();
 
 };
 #endif
